b655ae1526278776abdf3f51bdbe1d3ea0f8f606
anael <anael@192.168.0.10>
2020-11-21 19:21:59 -0300

    Modifiquei a classe da Ana Beatriz

--- src/model/appAnaBeatriz.java
+++ src/model/appAnaBeatriz.java
@@ -13,6 +13,7 @@ public class appAnaBeatriz {
     
     public static void main(String[] args) {
         System.out.println("Ana Beatriz da Silva");
+        System.out.println("Ana Eliza Feitoza");
     }
     
 }
